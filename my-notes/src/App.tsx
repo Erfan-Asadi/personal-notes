71e0871362d4980f3707040266380b624e33ee0b
Erfan Asadi <asadi.love7981@gmail.com>
2023-08-10 18:39:50 +0330

    create&use useLocalStorage custom hook

--- my-notes/src/App.tsx
+++ my-notes/src/App.tsx
@@ -2,20 +2,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
 import NewNote from "./components/NewNote";
+import { useLocalStorage } from "./hooks/useLocalStorage";
 
 export type Note = {
-  id: string & NoteData
-}
-export type NoteData = {
-  title: string,
-  markdown: string,
-  tags: Tag[]
-}
-export type Tag = {
+  id: string & NoteData;
+};
+
+export type RawNote = {
   id: string,
-  label: string
+  markdown: string,
+  tagIds: string[]
 }
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+export type Tag = {
+  id: string;
+  label: string;
+};
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", []);
+
   return (
     <Container className="my-4">
       <Routes>
--- my-notes/src/components/NoteForm.tsx
+++ my-notes/src/components/NoteForm.tsx
@@ -1,33 +1,32 @@
-import { useRef } from "react";
+import { SyntheticEvent, useRef, useState } from "react";
 import { Form, Stack, Col, Row, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
-import { NoteData } from "../App";
+import { NoteData, Tag } from "../App";
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
 };
 
+const defaultOptions = [
+  { value: "css", label: "CSS" },
+  { value: "html", label: "HTML" },
+  { value: "js", label: "JS" },
+  { value: "homework", label: "Homework" },
+];
 const NoteForm = ({ onSubmit }: NoteFormProps) => {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-
-  let defaultOptions = [
-    { value: "css", label: "CSS" },
-    { value: "html", label: "HTML" },
-    { value: "js", label: "JS" },
-    { value: "homework", label: "Homework" },
-  ];
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
 
     onSubmit({
-        title: titleRef.current!.value,
-        markdown: markdownRef.current!.value,
-        tags: []
-    })
-
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
   };
   return (
     <Form onSubmit={handleSubmit}>
@@ -42,7 +41,20 @@ const NoteForm = ({ onSubmit }: NoteFormProps) => {
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreatableReactSelect isMulti options={defaultOptions} />
+              <CreatableReactSelect
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                options={defaultOptions}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
