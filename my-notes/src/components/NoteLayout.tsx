34c8a4ba96af9e780e64605e0f5edf4882f18c5c
Erfan Asadi <asadi.love7981@gmail.com>
2023-08-11 23:34:52 +0330

    add Note, NoteLayout Components

---
+++ my-notes/src/components/NoteLayout.tsx
@@ -0,0 +1,20 @@
+import { Note, RawNote } from "../App";
+import { Navigate, Outlet, useOutletContext, useParams } from "react-router-dom";
+type NoteLayoutProps = {
+  notes: RawNote[];
+};
+const NoteLayout = ({ notes }: NoteLayoutProps) => {
+  const { id } = useParams();
+  const note = notes.find((note) => note.id === id);
+  
+  if (note == null) return <Navigate to="/" replace />;
+
+  return <Outlet context={note}/>;
+};
+
+
+export function useNote() {
+    return useOutletContext<Note>()
+}
+
+export default NoteLayout;
